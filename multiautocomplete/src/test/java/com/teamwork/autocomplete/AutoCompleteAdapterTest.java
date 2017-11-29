b45260070ce7cb10d857a5f30f748da3d4313f49
Marco Salis <fast3r@gmail.com>
2017-11-29 11:31:12 +0000

    AutoCompleteAdapterTest: using reflection instead of async waits for filter() to make tests reliable

--- multiautocomplete/src/test/java/com/teamwork/autocomplete/AutoCompleteAdapterTest.java
+++ multiautocomplete/src/test/java/com/teamwork/autocomplete/AutoCompleteAdapterTest.java
@@ -1,5 +1,7 @@
 package com.teamwork.autocomplete;
 
+import android.widget.Filter;
+
 import com.teamwork.autocomplete.adapter.AutoCompleteTypeAdapter;
 import com.teamwork.autocomplete.adapter.TypeAdapterDelegate;
 import com.teamwork.autocomplete.filter.SimpleTokenFilter;
@@ -13,6 +15,8 @@ import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -27,9 +31,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(RobolectricTestRunner.class)
-public class AutoCompleteAdapterTest {
-
-    private static final int ASYNC_FILTER_WAIT_MS = 300;
+public class AutoCompleteAdapterTest extends Filter {
 
     @Mock AutoCompleteViewBinder<String> viewBinder;
     @Mock MultiAutoComplete.Delayer delayer;
@@ -58,15 +60,16 @@ public class AutoCompleteAdapterTest {
     public void getCount() throws Exception {
         assertThat(autoCompleteAdapter.getCount(), is(0));
 
-        autoCompleteAdapter.getFilter().filter("it");
-        waitOnFilter();
+        Filter filter = autoCompleteAdapter.getFilter();
+        performFiltering(filter, "it");
+
         assertThat(autoCompleteAdapter.getCount(), is(2));
     }
 
     @Test
     public void getItem() throws Exception {
-        autoCompleteAdapter.getFilter().filter("it");
-        waitOnFilter();
+        Filter filter = autoCompleteAdapter.getFilter();
+        performFiltering(filter, "it");
 
         assertThat(autoCompleteAdapter.getItem(0), is("Italy"));
         assertThat(autoCompleteAdapter.getItem(1), is("United Kingdom"));
@@ -74,8 +77,8 @@ public class AutoCompleteAdapterTest {
 
     @Test
     public void getItemId() throws Exception {
-        autoCompleteAdapter.getFilter().filter("");
-        waitOnFilter();
+        Filter filter = autoCompleteAdapter.getFilter();
+        performFiltering(filter, "");
 
         assertThat(autoCompleteAdapter.getItemId(0), is((long) "Ireland".hashCode()));
         assertThat(autoCompleteAdapter.getItemId(3), is((long) "Spain".hashCode()));
@@ -85,9 +88,9 @@ public class AutoCompleteAdapterTest {
     public void getViewTypeCount() throws Exception {
         assertThat(autoCompleteAdapter.getViewTypeCount(), is(2));
 
-        //noinspection unchecked
-        ((TypeAdapterDelegate) typeAdapter).getFilter().performFiltering("it", dataset);
-        waitOnFilter();
+        Filter filter = autoCompleteAdapter.getFilter();
+        performFiltering(filter, "it");
+
         assertThat(autoCompleteAdapter.getViewTypeCount(), is(2));
     }
 
@@ -95,17 +98,18 @@ public class AutoCompleteAdapterTest {
     public void getItemViewType() throws Exception {
         assertThat(autoCompleteAdapter.getItemViewType(0), is(1));
 
-        autoCompleteAdapter.getFilter().filter("");
-        waitOnFilter();
+        Filter filter = autoCompleteAdapter.getFilter();
+        performFiltering(filter, "");
+
         assertThat(autoCompleteAdapter.getItemViewType(0), is(0));
     }
 
     @Test
     public void testFilter_performFiltering() throws Exception {
-        assertThat(autoCompleteAdapter.getFilter(), notNullValue());
+        Filter filter = autoCompleteAdapter.getFilter();
+        assertThat(filter, notNullValue());
 
-        autoCompleteAdapter.getFilter().filter("it");
-        waitOnFilter();
+        performFiltering(filter, "it");
 
         verify(((TypeAdapterDelegate) typeAdapter).getFilter()).stripHandle(eq("it"));
         verify(((TypeAdapterDelegate) typeAdapter).getFilter()).supportsToken("it");
@@ -113,8 +117,24 @@ public class AutoCompleteAdapterTest {
         verify(((TypeAdapterDelegate) typeAdapter).getFilter()).performFiltering("it", dataset);
     }
 
-    private void waitOnFilter() throws InterruptedException {
-        Thread.sleep(ASYNC_FILTER_WAIT_MS); // async filter
+    // running the asynchronous Filter.filter() yields to unstable tests which need to rely on Thread.sleep
+    private static void performFiltering(Filter filter, String constraint)
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<? extends Filter> filterCls = filter.getClass();
+        Method performFiltering = filterCls.getDeclaredMethod("performFiltering", CharSequence.class);
+        Method publishResults = filterCls.getDeclaredMethod("publishResults", CharSequence.class, FilterResults.class);
+
+        FilterResults results = (FilterResults) performFiltering.invoke(filter, constraint);
+        publishResults.invoke(filter, constraint, results);
     }
 
+    // extends Filter only for quicker access to FilterResults class
+
+    @Override protected FilterResults performFiltering(CharSequence charSequence) {
+        return null;
+    }
+
+    @Override protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+
+    }
 }
